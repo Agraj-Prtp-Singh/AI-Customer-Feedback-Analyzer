ddb5694615958ed5b182fba525d261b64bfc746a
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-08 15:15:30 +0545

    Implement clean structure

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+from services.ai_analyzer import analyze_feedback
+
+load_dotenv()
 
 app = FastAPI()
 
+
+class FeedbackRequest(BaseModel):
+    score: int
+    feedback: str
+
+
 @app.get("/")
 def home():
-    return {"message": "AI Customer Feedback Analyzer API is running."}
+    return {"message": "AI Customer Feedback Analyzer API is running"}
+
+
+@app.post("/analyze")
+def analyze(data: FeedbackRequest):
+
+    result = analyze_feedback(
+        data.score,
+        data.feedback
+    )
+
+    return result
