d94f451e9738496d9ac0d7f62df2e9cb400ae824
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-09 22:16:24 +0545

    feat: display follow-up management from app shell

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import Dashboard from "./components/Dashboard";
+import FollowUpManagement from "./components/FollowUpManagement";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <>
+      {/* <Dashboard /> */}
+      <FollowUpManagement />
+    </>
+  );
 }
 
 export default App;
